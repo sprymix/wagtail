dea183d86b0ba11ada04e2b6c1cd4011532f0c19
Victor Petrovykh <vpetrovykh@sprymix.com>
2014-08-12 18:22:20 -0400

    Make image cropper work with large images and add aspect ratio.

--- wagtail/wagtailimages/templates/wagtailimages/chooser/select_rendition.js
+++ wagtail/wagtailimages/templates/wagtailimages/chooser/select_rendition.js
@@ -1,20 +1,49 @@
 function(modal) {
+    // some large images are scaled down when displayed, so we need to account
+    // for that
+    var trueSize;
+    $(".crop-image img", modal.body).each(function(n, el) {
+        trueSize = [parseInt($(el).attr('width')),
+                    parseInt($(el).attr('height'))];
+    });
+
     var left = $('form input[name="left"]', modal.body),
         top = $('form input[name="top"]', modal.body),
         right = $('form input[name="right"]', modal.body),
         bottom = $('form input[name="bottom"]', modal.body);
 
 
+    // create a way to refer to the JCrop API
+    var jcapi;
+
     $(".crop-image img", modal.body).Jcrop({
-        onSelect: function(data) {
-            left.attr({value: data.x});
-            top.attr({value: data.y});
-            right.attr({value: data.x2});
-            bottom.attr({value: data.y2});
+        trueSize: trueSize
+    }, function() {
+        jcapi = this;
+    });
+
+    function applyCropValues() {
+        var data = jcapi.tellSelect();
+        left.attr({value: Math.round(data.x)});
+        top.attr({value: Math.round(data.y)});
+        right.attr({value: Math.round(data.x2)});
+        bottom.attr({value: Math.round(data.y2)});
+    };
+
+    var ratio_re = /(\d+):(\d+)/;
+    // change aspect ratio based on the radio selections
+    $('form input[name="aspect-ratio"]', modal.body).change(function(event) {
+        var match = ratio_re.exec(event.target.value);
+
+        if (match) {
+            jcapi.setOptions({
+                aspectRatio: parseInt(match[1]) / parseInt(match[2])
+            });
         }
     });
 
     $('form', modal.body).submit(function() {
+        applyCropValues();
         var formdata = new FormData(this);
 
         $.post(this.action, $(this).serialize(), function(response){
