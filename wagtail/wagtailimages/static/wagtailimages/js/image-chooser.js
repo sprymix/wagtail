11d096cb04cfd2fffb75a017b9bb2a4e036533d0
Vlad Pranskevichus <vlprans@gmail.com>
2014-08-14 16:33:04 +0300

    Image chooser: trigger change event when image is chosen

--- wagtail/wagtailimages/static/wagtailimages/js/image-chooser.js
+++ wagtail/wagtailimages/static/wagtailimages/js/image-chooser.js
@@ -32,26 +32,32 @@ function createRenditionChooser(id) {
     var previewImage = chooserElement.find('.preview-image img');
     var input = $('#' + id);
 
+    input.change(function(ev, imageData) {
+        if(imageData) {
+          previewImage.attr({
+            'src': imageData.preview.url,
+            'width': imageData.preview.width,
+            'height': imageData.preview.height,
+            'alt': imageData.title
+          });
+          chooserElement.removeClass('blank');
+        } else {
+          chooserElement.addClass('blank');
+        }
+    });
+
     $('.action-choose', chooserElement).click(function() {
         ModalWorkflow({
             'url': window.chooserUrls.imageChooser + '?select_rendition=True',
             'responses': {
                 'imageChosen': function(imageData) {
-                    input.val(imageData.id);
-                    previewImage.attr({
-                        'src': imageData.preview.url,
-                        'width': imageData.preview.width,
-                        'height': imageData.preview.height,
-                        'alt': imageData.title
-                    });
-                    chooserElement.removeClass('blank');
+                    input.val(imageData.id).trigger('change', imageData);
                 }
             }
         });
     });
 
     $('.action-clear', chooserElement).click(function() {
-        input.val('');
-        chooserElement.addClass('blank');
+        input.val('').trigger('change');
     });
 }
