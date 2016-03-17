f34955564b1393cae1a5add79587e99bca778310
Elvis Pranskevichus <elvis@magic.io>
2016-03-17 17:43:33 -0400

    Bump version

--- wagtail/wagtailcore/__init__.py
+++ wagtail/wagtailcore/__init__.py
@@ -1,6 +1,6 @@
-__version__ = '1.4.100'
+__version__ = '1.4.100.1'
 # Required for npm package for frontend
-__semver__ = '1.4.100'
+__semver__ = '1.4.100.1'
 default_app_config = 'wagtail.wagtailcore.apps.WagtailCoreAppConfig'
 
 
