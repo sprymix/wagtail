b7cdf435b0c8c2fe9c3127a296062e8b4efd8379
Elvis Pranskevichus <elvis@magic.io>
2016-04-28 17:01:30 -0400

    inline panel: Show only "Add Multiple" or "Add", but never both

--- wagtail/wagtailcore/__init__.py
+++ wagtail/wagtailcore/__init__.py
@@ -1,6 +1,6 @@
-__version__ = '1.4.100.28'
+__version__ = '1.4.100.29'
 # Required for npm package for frontend
-__semver__ = '1.4.100.28'
+__semver__ = '1.4.100.29'
 default_app_config = 'wagtail.wagtailcore.apps.WagtailCoreAppConfig'
 
 
