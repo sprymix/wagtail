fb135befe101f333396416fe695eff755db566d5
Elvis Pranskevichus <elvis@magic.io>
2016-05-31 16:54:01 -0400

    Unbreak page sorting in explorer

--- wagtail/wagtailcore/__init__.py
+++ wagtail/wagtailcore/__init__.py
@@ -1,6 +1,6 @@
-__version__ = '1.4.100.29'
+__version__ = '1.4.100.30'
 # Required for npm package for frontend
-__semver__ = '1.4.100.29'
+__semver__ = '1.4.100.30'
 default_app_config = 'wagtail.wagtailcore.apps.WagtailCoreAppConfig'
 
 
